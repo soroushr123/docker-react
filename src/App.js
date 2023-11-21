667fec14d88dc14bb38b2cbcba0e088dc3f47b44
soroushr123 <soroushrahimi.eng@gmail.com>
2023-11-20 21:34:18 -0800

    changed feature

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Change in feature branch
         </a>
       </header>
     </div>
