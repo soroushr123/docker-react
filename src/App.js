6c600d0217591b348d9920d6bca2536aee5cb491
soroushr123 <soroushrahimi.eng@gmail.com>
2023-11-20 21:52:12 -0800

    changed feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          i was changed
+          feature changed
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Change in feature branch
+          Learn React
         </a>
       </header>
     </div>
